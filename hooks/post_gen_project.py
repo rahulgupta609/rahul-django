056f88863a5d4225d217e1e69f4434ffdc7f5cba
AkritiKapur <akritikapur1@gmail.com>
2016-11-29 16:49:18 +0530

    adding templatised deployment necessary files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,46 @@
 # ACTIONS TO BE DONE AFTER THE PROJECT IS COMPLETED.
+
+import os
+import shutil
+
+# Get the root project directory
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+def remove_heroku_files():
+    """
+    Removes files needed for heroku
+    """
+    filenames = ["Procfile", "runtime.txt"]
+    for filename in filenames:
+        file_name = os.path.join(PROJECT_DIRECTORY, filename)
+        remove_file(file_name)
+
+
+def remove_elasticbeanstalk():
+    """
+    Removes elastic beanstalk components
+    """
+    docs_dir_location = os.path.join(PROJECT_DIRECTORY, '.ebextensions')
+    if os.path.exists(docs_dir_location):
+        shutil.rmtree(docs_dir_location)
+
+    filenames = ["ebsetenv.py"]
+    for filename in filenames:
+        remove_file(os.path.join(
+            PROJECT_DIRECTORY, filename
+        ))
+
+
+# Removes heroku files
+if '{{ cookiecutter.use_heroku }}'.lower() != 'y':
+    remove_heroku_files()
+
+# Remove Elastic Beanstalk files
+if '{{ cookiecutter.use_elasticbeanstalk }}'.lower() != 'y':
+    remove_elasticbeanstalk()
