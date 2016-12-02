1cf2a9631eeb618a88d2e6d04d68fd97495605b1
Sidharth Chhawchharia <sidharth.c@hashedin.com>
2016-12-02 15:24:49 +0530

    Temporarily removed key genration script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -151,7 +151,7 @@ def perform_post_gen_action():
     Performs actions needed after a Django project has been created.
     """
     # 1. Generates and saves random secret key
-    make_secret_key(PROJECT_DIRECTORY)
+    # make_secret_key(PROJECT_DIRECTORY)
 
     # Removes heroku files
     if '{{ cookiecutter.use_heroku }}'.lower() != 'y':
